6052771c1ff89021b403fa2e3fd0317215b21bb5
JuanGR-FI <juan60430@hotmail.com>
2022-11-06 00:43:24 -0600

    Agregando Date Picker

--- app/src/main/java/com/example/myhoroscope/MainActivity.kt
+++ app/src/main/java/com/example/myhoroscope/MainActivity.kt
@@ -16,36 +16,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.etDatePicker.setOnClickListener{
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment {day,month,year -> onDateSelected(day,month,year)}
+        datePicker.show(supportFragmentManager,"datePicker")
+    }
+
+    fun onDateSelected(day: Int, month: Int, year: Int){
+        binding.etDatePicker.setText("$day/$month/$year")
+        //binding.etDatePicker.text = "$day/$month/$year"
     }
 
     fun click(view: View) {
+        flag = true
         if(binding.etNombre.text.isNotEmpty()){
             val nombre = binding.etNombre.text.toString()
         }else{
             Toast.makeText(this@MainActivity,"Por favor ingresa tu nombre",Toast.LENGTH_SHORT).show()
             binding.etNombre.error = "Se requiere un nombre"
+            flag = false
+        }
+        if(binding.etDatePicker.text.isEmpty()){
+            Toast.makeText(this@MainActivity,"Por favor ingresa una fecha de nacimiento",Toast.LENGTH_SHORT).show()
+            binding.etDatePicker.error = "Se requiere una fecha de nacimiento"
+            flag = false
         }
         if(binding.etNumCuenta.text.isNotEmpty()){
             val nc = binding.etNumCuenta.text.toString()
             if(nc.length != 9){
                 Toast.makeText(this@MainActivity,"Número de cuenta incompleto",Toast.LENGTH_SHORT).show()
                 binding.etNumCuenta.error = "Se requiere un número de cuenta de 9 dígitos"
+                flag = false
             }else{
                 val nc = nc.toInt()
             }
         }else{
             Toast.makeText(this@MainActivity,"Por favor ingresa tu número de cuenta",Toast.LENGTH_SHORT).show()
             binding.etNumCuenta.error = "Se requiere un número de cuenta"
+            flag = false
         }
         if(binding.etCorreo.text.isNotEmpty()){
             val correo = binding.etCorreo.text.toString()
             if(!PatternsCompat.EMAIL_ADDRESS.matcher(correo).matches()){
                 Toast.makeText(this@MainActivity,"Por favor ingresa un correo electrónico válido",Toast.LENGTH_SHORT).show()
                 binding.etCorreo.error = "Se requiere un correo electrónico válido"
+                flag = false
             }
         }else{
             Toast.makeText(this@MainActivity,"Por favor ingresa un correo electrónico",Toast.LENGTH_SHORT).show()
             binding.etCorreo.error = "Se requiere un correo electrónico"
+            flag = false
+        }
+
+        if(flag){
+            Toast.makeText(this@MainActivity,"Todo correcto",Toast.LENGTH_SHORT).show()
         }
     }
 
